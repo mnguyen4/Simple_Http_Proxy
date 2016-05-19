8639a1c1bfd20e232e9b647c083ada36d059c477
Minh Nguyen <minov88@gmail.com>
2016-05-19 08:07:23 -0700

    Initial commit
    Added empty function to read blacklist.txt.
    Still need to determine the best way to store blacklist content in
    memory.

---
+++ Simple_Http_Proxy/Utils/BlacklistUtil.cs
@@ -0,0 +1,17 @@
+﻿using Simple_Http_Proxy.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Http_Proxy.Utils
+{
+    class BlacklistUtil
+    {
+        public static void readBlacklist()
+        {
+
+        }
+    }
+}
