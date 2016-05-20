5565cb4f404a4228001783834ff5c86a887a23fb
Minh Nguyen <minov88@gmail.com>
2016-05-19 23:18:35 -0700

    Added blacklist read function.
    Added Constant to easily access preferences.
    Added function to read the blacklist file and store the data into
    AppStorage.

---
+++ Simple_Http_Proxy/Constants/Constant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Http_Proxy.Constants
+{
+    class Constant
+    {
+        public const string BLACK_LOCATION_TEXT = "blackLocationTxt";
+        public const string WHITE_LOCATION_TEXT = "whiteLocationTxt";
+    }
+}
--- Simple_Http_Proxy/Utils/BlacklistUtil.cs
+++ Simple_Http_Proxy/Utils/BlacklistUtil.cs
@@ -1,6 +1,8 @@
-﻿using Simple_Http_Proxy.Memory;
+﻿using Simple_Http_Proxy.Constants;
+using Simple_Http_Proxy.Memory;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,17 @@ namespace Simple_Http_Proxy.Utils
     {
         public static void readBlacklist()
         {
-
+            AppStorage storage = AppStorage.getInstance();
+            if (File.Exists((string)storage.getPreference(Constant.BLACK_LOCATION_TEXT)))
+            {
+                string blacklistItem = "";
+                StreamReader blacklistFile = new StreamReader((string)storage.getPreference(Constant.BLACK_LOCATION_TEXT));
+                while ((blacklistItem = blacklistFile.ReadLine()) != null)
+                {
+                    storage.addBlacklistItem(blacklistItem);
+                }
+                blacklistFile.Close();
+            }
         }
     }
 }
