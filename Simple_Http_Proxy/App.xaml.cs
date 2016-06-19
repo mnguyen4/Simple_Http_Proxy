c9adcf4d3fd28cd8b2bb1156d69a1bb9575aa551
Minh Nguyen <minov88@gmail.com>
2016-06-19 00:53:27 -0700

    Added tray icon.
    Added icon image.
    Added tray icon display display on startup.
    Added tray icon dispose on exit.

--- Simple_Http_Proxy/App.xaml.cs
+++ Simple_Http_Proxy/App.xaml.cs
@@ -14,10 +14,20 @@ namespace Simple_Http_Proxy
     /// </summary>
     public partial class App : Application
     {
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+
+        private void onApplicationStartup(object sender, StartupEventArgs e)
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("Images\\main.ico");
+            notifyIcon.Visible = true;
+        }
+
         private void onApplicationExit(object sender, ExitEventArgs e)
         {
             HttpProxyListener proxyListener = HttpProxyListener.getInstance();
             proxyListener.stopListener();
+            notifyIcon.Dispose();
         }
     }
 }
